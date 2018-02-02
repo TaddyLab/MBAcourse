7845be84f8317738b6260446408e45c9cd080e4f
mataddy <mataddy@gmail.com>
2018-02-02 14:34:11 -0800

    small

--- examples/dominicks-beer.R
+++ examples/dominicks-beer.R
@@ -21,20 +21,24 @@ xu <- sparse.model.matrix( ~ u-1, data=wber)
 xw <- sparse.model.matrix( ~ w-1, data=wber)
 controls <- cBind(xs, xu, xw, descr[wber$UPC,]) 
 
+# full data mle fit
+fullfit <- gamlr(x=cBind(lp=wber$lp,controls), y=log(wber$MOVE), lambda.start=0)
+print( coef(fullfit)[1:2,] )
+
 # smallbeer
-set.seed(5807)
-ss <- sample.int(nrow(wber),1e4)
+set.seed(888)
+ss <- sample.int(nrow(wber),5e3)
 
 # all together
 coef( margfit <- lm(log(MOVE) ~ lp, data=wber[ss,]) )
 
 # mle
 mlefit <- gamlr(x=cBind(lp=wber$lp,controls)[ss,], y=log(wber$MOVE)[ss], lambda.start=0)
-coef(mlefit)[1:2,]
+print( coef(mlefit)[1:2,] )
 
 # naive lasso
 naivefit <- gamlr(x=cBind(lp=wber$lp,controls)[ss,], y=log(wber$MOVE)[ss], free=1, standardize=FALSE)
-coef(naivefit)[1:2,]
+print( coef(naivefit)[1:2,] )
 
 # double ML
 source("orthoML.R")
@@ -46,29 +50,37 @@ yreg <- function(x,y){
 
 resids <- orthoPLTE( x=controls[ss,], d=wber$lp[ss], y=log(wber$MOVE)[ss], dreg=dreg, yreg=yreg, nfold=5)
 
-# full data mle fit
-fullfit <- gamlr(x=cBind(lp=wber$lp,controls), y=log(wber$MOVE), lambda.start=0)
-coef(fullfit)[1:2,]
-
 ##############  heterogeneity
 
+# interact items and text with price
 lpxu <- xu*wber$lp
 colnames(lpxu) <- paste("lp",colnames(lpxu),sep="")
-
-# mle with all upcs
-mlehte <- gamlr(x=cBind(controls,lpxu)[ss,], y=log(wber$MOVE)[ss], lambda.start=0)
-gammle <- coef(mlehte)[-(1:(ncol(controls)+1)),]
-pdf("smallbeer-mle.pdf", width=4, height=4)
-par(mai=c(.9,.9,.1,.1))
-hist(gammle, main="", xlab="elasticity", col="pink", freq=FALSE)
-dev.off()
-sort(gammle)[1:4]
 
 xhte <- cBind(1,xu,descr[wber$UPC,])
 colnames(xhte)[1] <- "(baseline)"
 d <- xhte*wber$lp
 colnames(d) <- paste("lp",colnames(d),sep=":")
 oxhte <- xhte[match(rownames(upc),wber$UPC),]
+rownames(oxhte) <- rownames(upc)
+
+# fullhte 
+fullhte <- gamlr(x=cBind(d,controls), y=log(wber$MOVE), lambda.start=0)
+gamfull <- drop(oxhte%*%coef(fullhte)[2:(ncol(d)+1),])
+
+pdf("smallbeer-full.pdf", width=4, height=4)
+par(mai=c(.9,.9,.1,.1))
+hist(gamfull, main="", xlab="elasticity", ,
+			 col="darkgrey", freq=FALSE)
+dev.off()
+
+# mle with all upcs
+mlehte <- gamlr(x=cBind(d,controls)[ss,], y=log(wber$MOVE)[ss], lambda.start=0)
+gammle <- drop(oxhte%*%coef(fullhte)[2:(ncol(d)+1),])
+pdf("smallbeer-mle.pdf", width=4, height=4)
+par(mai=c(.9,.9,.1,.1))
+hist(gammle, main="", xlab="elasticity", col="pink", ylim= freq=FALSE)
+dev.off()
+sort(gammle)[1:4]
 
 # naive fit
 naivehte <- gamlr(x=cBind(d,controls)[ss,], y=log(wber$MOVE)[ss], free=1, standardize=FALSE)
@@ -82,28 +94,19 @@ dev.off()
 # double ML
 dmlhte <- gamlr(x=xhte[ss,]*resids$dtil, y=resids$ytil, free=1, standardize=FALSE)
 coef(dmlhte)[1:2]
-gamdml <- drop(oxhte%*%coef(dmlhte)[-1,])
+range( gamdml <- drop(oxhte%*%coef(dmlhte)[-1,]) )
 pdf("smallbeer-dml.pdf", width=4, height=4)
 par(mai=c(.9,.9,.1,.1))
 hist(gamdml, main="", xlab="elasticity", col="lightblue", freq=FALSE)
 dev.off()
 
-# fullhte 
-fullhte <- gamlr(x=cBind(controls,lpxu), y=log(wber$MOVE), lambda.start=0)
-gamfull <- coef(fullhte)[-(1:(ncol(controls)+1)),]
-
-pdf("smallbeer-full.pdf", width=4, height=4)
-par(mai=c(.9,.9,.1,.1))
-hist(gamfull, main="", xlab="elasticity", ,
-			 col="darkgrey", freq=FALSE)
-dev.off()
-
-pdf("smallbeer-compare.pdf", width=8, height=4)
-par(mai=c(.9,.9,.1,.1), mfrow=c(1,3))
-plot(gamfull, gammle, pch=21, bg=8, xlab="fulldata MLE", ylab="subsample MLE")
-abline(a=0, b=1, lty=2)
-plot(gamfull, gamnaive, pch=21, bg=8, xlab="fulldata MLE", ylab="subsample Naive ML")
-abline(a=0, b=1, lty=2)
-plot(gamfull, gamdml, pch=21, bg=8, xlab="fulldata MLE", ylab="subsample DML")
-abline(a=0, b=1, lty=2)
+ylim <- c(-10,2)
+pdf("smallbeer-compare.pdf", width=8, height=3)
+par(mai=c(.7,.7,.1,.1), mfrow=c(1,3))
+plot(gamfull, gammle, pch=21, bg="pink", xlab="fulldata MLE", ylab="subsample MLE", bty="n", ylim=c(-60,60))
+text(x=-1,y=15, sprintf("R2 = %.02f",summary(lm(gamfull~gammle))$r.squared))
+plot(gamfull, gamnaive, pch=21, bg="lightyellow", xlab="fulldata MLE", ylab="subsample Naive ML", bty="n", ylim=ylim)
+text(x=-1,y=15, sprintf("R2 = %.02f",summary(lm(gamfull~gamnaive))$r.squared))
+plot(gamfull, gamdml, pch=21, bg="lightblue", xlab="fulldata MLE", ylab="subsample Double ML", bty="n", ylim=ylim)
+text(x=-1,y=15, sprintf("R2 = %.02f",summary(lm(gamfull~gamdml))$r.squared))
 dev.off()
